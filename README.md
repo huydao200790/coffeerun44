4e93d00cfd5deac2ff14a4ff3080e65bfe638610
Huy Dao <huydao200790@gmail.com>
2018-03-12 00:42:58 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # coffeerun
+# coffeerun4
--- scripts/paymentscript.js
+++ scripts/paymentscript.js
@@ -5,5 +5,13 @@
 
   var FormHandler = App.FormHandler;
   var formHandler1 = new FormHandler(FORM_PAYMENT);
-  formHandler1.addSubmitHandler(function(data) {});
+  formHandler1.addSubmitHandler(function(data) {
+    $('[manual-ajax]').click(function(event) {
+  event.preventDefault();
+  this.blur(); // Manually remove focus from clicked link.
+  $.get(this.href, function(html) {
+    $(html).appendTo('body').modal();
+  });
+});
+  });
 })(window);
