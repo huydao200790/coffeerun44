449dbf702c71c31ec8f868a5caa11819787387b5
huydao <huydao200790@gmail.com>
2018-03-12 23:50:27 -0700

    final commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
   'use strict';
   var FORM_SELECTOR ='[data-coffee-order="form"]';
   var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-  var SERVER_URL =" http://localhost:2403/coffeeorders"
+  var SERVER_URL ="http://localhost:2403/coffeeorders"
 // "http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders";
 
   var App = window.App;
@@ -12,14 +12,15 @@
   var FormHandler = App.FormHandler;
   var Validation = App.Validation;
   var CheckList = App.CheckList;
+  
   var remoteDS = new RemoteDataStore(SERVER_URL);
   var myTruck = new Truck('ncc-1701', remoteDS);
   window.myTruck = myTruck;
+
   var checkList = new CheckList(CHECKLIST_SELECTOR);
   checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+
   var formHandler = new FormHandler(FORM_SELECTOR);
-
-
   formHandler.addSubmitHandler(function(data) {
   myTruck.createOrder(data);
   checkList.addRow(data);
--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -1,4 +1,5 @@
 (function(window){
+  
   'use strict'
   var App = window.App || {};
   var $ = window.jQuery;
@@ -8,7 +9,7 @@
     }
 
     this.serverURL = url;
-  }
+   }
 
   RemoteDataStore.prototype.add = function(key,val){
     $.post(this.serverURL,val,function(serverResponse){
@@ -24,14 +25,39 @@
   };
 
   RemoteDataStore.prototype.get = function(key,cb){
+    /*
     $.get(this.serverURL + '/' +key,function(serverResponse){
       console.log(serverResponse);
       cb(serverResponse);
-    })
+    })*/
+    $.ajax({
+      url:this.serverURL,
+      type:'GET',
+      data:{emailAddress:key},
+      dataType:'json',
+      cache:'false',
+      success:function(a){
+        //console.log(a[0]);
+         
+      },
+    });
+   
   };
-
+  
   RemoteDataStore.prototype.remove = function(key){
-    $.ajax(this.serverURL + '/' + key,{type: 'DELETE'});
+      $.ajax({
+      url:this.serverURL,
+      type:'GET',
+      data:{emailAddress:key},
+      dataType:'json',
+      cache:'false',
+      success:function(a){
+        var myid = a[0].id;
+        $.ajax("http://localhost:2403/coffeeorders"+myid,{type: 'DELETE'}); //this.serverURL does not work
+        console.log(myid);
+      },
+    });
+     
   };
 
   App.RemoteDataStore = RemoteDataStore;
